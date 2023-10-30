Sammon error calculation bug fixed

MainTask/PSO.py
@@ -60,7 +60,7 @@ def PSO(x, optim_func, n_features, max_iter=30, n_particles=30, w=0.6, c1=2, c2=
             d_x = torch.cdist(x[:, best_features_idx[idx]], x[:, best_features_idx[idx]])
             d_x = d_x.view(-1)[tril_index]
             
-            error[idx, 0] = optim_func(d_x, d_y)
+            error[idx, 0] = optim_func(d_y, d_x)
         #error = optim_func(x_stacked_indexed, x)
         
         # Update personal best
@@ -84,6 +84,6 @@ def PSO(x, optim_func, n_features, max_iter=30, n_particles=30, w=0.6, c1=2, c2=
 
         particles += velocities
         
-        error_log.append(gbest_val)
+        error_log.append(gbest_val.cpu().item())
     
-    return torch.topk(gbest_pos, n_features, dim=1)[1].squeeze(), error_log
+    return torch.topk(gbest_pos, n_features, dim=1)[1].squeeze().cpu(), error_log

MainTask/utils.py
@@ -2,8 +2,8 @@ import torch
 
 def sammon_error(d_x, d_y, smooth=1e-8):
     '''
-    :param d_x:     Input data lower-triangular matrix of distances -> Tensor[#n classes/functions, #n classes/functions]
-    :param d_y:     Original data distance must be lower-triangular using torch.tril() -> Tensor[#n classes/functions, #n classes/functions]
+    :param d_x:     Original data distance must be lower-triangular using torch.tril() -> Tensor[#n classes/functions, #n classes/functions]
+    :param d_y:     Input data lower-triangular matrix of distances -> Tensor[#n classes/functions, #n classes/functions]
     :param device:  Device cpu or cuda
     :param smooth:  Smooth to avoid div by 0
     '''